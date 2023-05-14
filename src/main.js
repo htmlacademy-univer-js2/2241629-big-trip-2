d3128013e1e68eeade9dd215e910d244c6ff896f
nasastina <bmktaa45@gmail.com>
2023-05-14 21:29:11 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import FiltersView from './view/filter-view.js';
-import { render,} from './render.js';
 import BoardPresenter from './presenter/board-presenter.js';
-import SortView from './view/sort-view.js';
+import PointsModel from './model/points-model.js';
+import MainPresenter from './presenter/main-presenter.js';
 
-const bodyElement = document.querySelector('.page-body');
-const filtersElement = bodyElement.querySelector('.trip-controls__filters');
-const sortElement = bodyElement.querySelector('.trip-events');
-const boardPresenter = new BoardPresenter({boardContainer: sortElement});
+const headerElement = document.querySelector('.page-header__container');
+const pageBodyElement = document.querySelector('.page-body__page-main');
 
-render(new SortView(), sortElement);
-render(new FiltersView(), filtersElement);
+const pageBodyContainerElement = pageBodyElement.querySelector('.page-body__container');
+const pointsModel = new PointsModel();
+const boardPresenter = new BoardPresenter({container: pageBodyContainerElement, pointsModel});
+const mainPresenter = new MainPresenter({container: headerElement});
 
+mainPresenter.init();
 boardPresenter.init();
