4277df41f827289f8abc5c3e76474f991a00f4c9
nasastina <bmktaa45@gmail.com>
2023-06-08 16:06:10 +0500

    Революция или эволюция? (часть 1)

--- src/view/filter-view.js
+++ src/view/filter-view.js
@@ -1,33 +1,37 @@
-import {createElement} from '../render.js';
+import { createElement } from '../render.js';
 
-function createTemplate() {
-  return (`<form class="trip-filters" action="#" method="get">
-    <div class="trip-filters__filter">
-      <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-      <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-    </div>
-    <div class="trip-filters__filter">
-      <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-      <label class="trip-filters__filter-label" for="filter-future">Future</label>
-    </div>
-    <button class="visually-hidden" type="submit">Accept filter</button>
-  </form>`
-);
-}
+const createFiltersTemplate = () => `<form class="trip-filters" action="#" method="get">
+<div class="trip-filters__filter">
+  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
+  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
+</div>
+<div class="trip-filters__filter">
+  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
+  <label class="trip-filters__filter-label" for="filter-future">Future</label>
+</div>
+<div class="trip-filters__filter">
+  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
+  <label class="trip-filters__filter-label" for="filter-past">Past</label>
+</div>
+<button class="visually-hidden" type="submit">Accept filter</button>
+</form>`;
 
-export default class FilterView {
-  getTemplate() {
-    return createTemplate();
+export default class FiltersView {
+  #element =null;
+
+  get template () {
+    return createFiltersTemplate();
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element){
+      this.#element = createElement(this.template);
     }
-    return this.element;
+
+    return this.#element;
   }
 
   removeElement() {
-    this.element = null;
+    this.#element = null;
   }
 }
